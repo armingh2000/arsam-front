c4945a180fe61568a4085aa65d8a2a4d735f23bc
armingh2000 <armingholampour71@gmail.com>
2020-10-24 21:55:41 -0400

    removing login files to change to initial state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import Login from "./view/scripts/pages/login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
     </div>
   );
 }
