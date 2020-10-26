ed56d6193bce8c6b432dd10625921bb12abf1fb8
armingh2000 <armingholampour71@gmail.com>
2020-10-25 23:42:35 -0400

    antd scss packages added, login css completed

--- src/view/scripts/pages/login/components/NormalLoginForm.jsx
+++ src/view/scripts/pages/login/components/NormalLoginForm.jsx
@@ -1,95 +1,71 @@
-import React, { useState } from "react";
-import 'antd/dist/antd.css';
+import React from "react";
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import axios from "axios";
 
 const NormalLoginForm = () => {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
-    const [{username, password}, setUser] = useState({username:"", password:""});
 
-    function handleChange(event)
-    {
-        const {value, name} = event.target;
-        setUser((prevValue) => {
-            return ({
-                ...prevValue,
-                [name] : value
-            })
-        })
-    }
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+        props.form.resetFields()
+    };
 
-    function handleSubmit(event)
-    {
-        
-        console.log("handleSubmit")
-        setUser({username:"", password:""});
-    }
-
-  return (
-    <Form
-      onSubmit={handleSubmit}
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
+    return (
+        <Form
+        id="components-form-demo-normal-login"
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+            remember: true,
+        }}
+        onFinish={onFinish}
+        >
+        <Form.Item
         name="username"
         rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
+            {
+                required: true,
+                message: 'Please input your Username!',
+            },
         ]}
-      >
+        >
         <Input 
-          onChange={handleChange} 
-          value={username} 
-          name="username" 
-          prefix={<UserOutlined className="site-form-item-icon" />} 
-          placeholder="Username" />
-      </Form.Item>
-      <Form.Item
+        prefix={<UserOutlined className="site-form-item-icon" />} 
+        placeholder="Username" />
+        </Form.Item>
+        <Form.Item
         name="password"
         rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
+            {
+                required: true,
+                message: 'Please input your Password!',
+            },
         ]}
-      >
+        >
         <Input
-          onChange={handleChange}
-          value={password}
-          name="password"
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
+        prefix={<LockOutlined className="site-form-item-icon" />}
+        type="password"
+        placeholder="Password"
         />
-      </Form.Item>
-      <Form.Item>
+        </Form.Item>
+        <Form.Item>
         <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Checkbox>Remember me</Checkbox>
         </Form.Item>
 
         <a className="login-form-forgot" href="">
-          Forgot password
+        Forgot password
         </a>
-      </Form.Item>
+        </Form.Item>
 
-      <Form.Item>
+        <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
+        Log in
         </Button>
-         Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
-  );
+        Or <a href="">register now!</a>
+        </Form.Item>
+        </Form>
+    );
 };
 
 export default NormalLoginForm;
