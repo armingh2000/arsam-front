805d59e4200fa80002dfec6f091f11a4ccc0973f
Armin <armingh379@gmail.com>
2020-10-29 18:59:39 +0330

    Complete Login
    Add request, react-router, localStorage to NormalLoginForm
    
    Add initial files for Home, Signup pages
    
    Add router, switch in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Login from "./view/scripts/pages/login/Login";
+import Home from "./view/scripts/pages/home/Home";
+import Signup from "./view/scripts/pages/signup/Signup";
 import 'antd/dist/antd.css';
 import "./view/styles/main.scss";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <Login />
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/" component={Home}/>
+      </Switch>
     </div>
-  );
+  </Router>);
 }
 
 export default App;
--- src/view/scripts/pages/login/Login.jsx
+++ src/view/scripts/pages/login/Login.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import NormalLoginForm from "./components/NormalLoginForm";
-import { Row, Col } from 'antd';
+import {Row, Col} from 'antd';
 
-function Login()
-{
-    return (
-        <div>
-            <Row style={{minHeight: '100vh'}} justify="center" align="middle">
-                <Col span={6}><NormalLoginForm /></Col>
-            </Row>
-        </div>
-    )
+function Login() {
+  return (<div>
+    <Row style={{
+        minHeight: '100vh'
+      }} justify="center" align="middle">
+      <Col span={6}><NormalLoginForm/></Col>
+    </Row>
+  </div>)
 }
 
 export default Login;
