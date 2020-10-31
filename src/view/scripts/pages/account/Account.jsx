e6394667c666e8e8c3252cf418614ed7fa04ea9b
armin <45814289+armingh2000@users.noreply.github.com>
2020-10-31 21:18:32 +0330

    Revert "Feature/login signup style"

--- src/view/scripts/pages/account/Account.jsx
+++ src/view/scripts/pages/account/Account.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import GLogout from "../login/components/GLogout";
+
 
 const Account = () =>
 {
-  return(<div>
-    <p>{localStorage.getItem("userToken")}</p>
-    <GLogout />
-  </div>);
+  return <p>{localStorage.getItem("userToken")}</p>;
 };
 
 export default Account;
--- src/view/scripts/pages/login/Login.jsx
+++ src/view/scripts/pages/login/Login.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import NormalLoginForm from "./components/NormalLoginForm";
+import GLogin from "./components/GLogin";
+import GLogout from "./components/GLogout";
 import {Row, Col} from 'antd';
 
 function Login() {
-  return (<div id="components-form-demo-normal-login">
+  return (<div>
     <Row style={{
         minHeight: '100vh'
       }} justify="center" align="middle">
       <Col span={6}>
         <NormalLoginForm/>
+        {localStorage.getItem("userToken") ? <GLogout /> : <GLogin />}
       </Col>
     </Row>
   </div>)
