007c22ff177903ee6aedaea8507840371a1726e1
armin <45814289+armingh2000@users.noreply.github.com>
2020-10-31 19:00:08 +0330

    Revert "Feature/login signup style"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React from "react";
-import Login from "./view/scripts/pages/login/Login";
-import Home from "./view/scripts/pages/home/Home";
-import Signup from "./view/scripts/pages/signup/Signup";
-import Account from "./view/scripts/pages/account/Account";
-import 'antd/dist/antd.css';
-import "./view/styles/main.scss";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { useState } from "react";
 
 function App() {
-  return (<Router>
+  return (
     <div className="App">
-      <Switch>
-        <Route path="/login" component={Login}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/account" component={Account}/>
-        <Route path="/" component={Home}/>
-      </Switch>
     </div>
-  </Router>);
+  );
 }
 
 export default App;
