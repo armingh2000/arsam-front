a80aa11239131fc8196e8854319d81d8e28d8283
armingh2000 <armingh379@gmail.com>
2020-11-07 14:08:57 -0500

    resolve path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./view/scripts/pages/login/Login";
 import Home from "./view/scripts/pages/home/Home";
 import Signup from "./view/scripts/pages/signup/Signup";
 import Account from "./view/scripts/pages/account/Account";
-import ShowEvent from "./view/scripts/pages/show-event/ShowEvent";
+import ShowEvent from "./view/scripts/pages/event/show-event/ShowEvent";
 import 'antd/dist/antd.css';
 import "./view/styles/main.scss";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
