9a0781a460291b4a8a7ca938efd78f8cae993c0e
Armin <armingh379@gmail.com>
2020-11-12 23:20:31 +0330

    Change carousel dots pisition

--- src/view/scripts/pages/event/show-event/components/EventImages.jsx
+++ src/view/scripts/pages/event/show-event/components/EventImages.jsx
@@ -5,15 +5,16 @@ import {
 
 const EventImages = (props) =>
 {
+
+
     const imagesArray = props.images;
-    return (
-        <Carousel autoplay  autoplaySpeed="50" speed="1000" fade dotPosition="left" width="100%">
-        {imagesArray.map((img) => {
-            return <div>
-            <Image src={img.src} width="100%" alt="event image"/>
-            </div>
-        })}
-        </Carousel>
+    return (<Carousel autoplay dotPosition="top" autoplaySpeed="50" speed="1000" fade width="100%">
+          {imagesArray.map((img) => {
+              return <div>
+              <Image src={img.src} width="100%" alt="event image"/>
+              </div>
+          })}
+          </Carousel>
     );
 
 }
