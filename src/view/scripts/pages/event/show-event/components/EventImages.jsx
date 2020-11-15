7e2a24cbedddc5e12eade1e3cd8f333333e39c82
Armin <armingh379@gmail.com>
2020-11-16 00:21:01 +0330

    Fix image div responsive

--- src/view/scripts/pages/event/show-event/components/EventImages.jsx
+++ src/view/scripts/pages/event/show-event/components/EventImages.jsx
@@ -10,7 +10,7 @@ const EventImages = (props) =>
     const imagesArray = props.images;
     return (<Carousel autoplay autoplaySpeed="50" speed="1000" fade className="carousel">
           {imagesArray.map((img) => {
-              return <div>
+              return <div className="image-div">
               <Image src={img.src}  alt="event image"/>
               </div>
           })}
