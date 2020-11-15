96833a947e25b2a072daf52ffe1e38cb45c40db9
Armin <armingh379@gmail.com>
2020-11-15 23:59:53 +0330

    Add time, icon, tooltip

--- src/view/scripts/pages/event/show-event/components/EventDetails.jsx
+++ src/view/scripts/pages/event/show-event/components/EventDetails.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { Typography, Tag, Col, Row } from 'antd';
+import {
+  ClockCircleOutlined,
+  UserOutlined,
+  TrophyOutlined,
+  DollarCircleOutlined,
+  TeamOutlined,
+  SoundOutlined,
+  SafetyOutlined,
+  FireOutlined
+} from '@ant-design/icons';
 
 
 const EventDetails = ({name, startDate, endDate, creator, categories}) =>
@@ -10,12 +20,12 @@ const EventDetails = ({name, startDate, endDate, creator, categories}) =>
   return (<div align="top" className="event-details">
     <div className="details-header">
       <Title level={2}>{name}</Title>
-      <Title type="secondary" level={5}>{creator.email}</Title>
+      <Title type="secondary" level={5}><UserOutlined /> {creator.email}</Title>
     </div>
     <br/>
     <Row justify="space-around" align="middle" gutter={[8,8]}>
       <Col>
-        <Text className="text">Start
+        <Text className="text"><ClockCircleOutlined /> Start
           <br/>
           {new Date(startDate).toDateString()}
           <br/>
@@ -23,7 +33,7 @@ const EventDetails = ({name, startDate, endDate, creator, categories}) =>
         </Text>
       </Col>
       <Col>
-        <Text className="text">End
+        <Text className="text"><ClockCircleOutlined /> End
           <br/>
           {new Date(endDate).toDateString()}
           <br/>
@@ -36,17 +46,17 @@ const EventDetails = ({name, startDate, endDate, creator, categories}) =>
     <Text className="text tags">Tags: {categories.map((tag) => {
         switch (tag) {
           case 1:
-            return <Tag color="magenta">Race</Tag>;
+            return <Tag color="magenta"><TrophyOutlined /> Race</Tag>;
           case 2:
-            return <Tag color="gold">Performance</Tag>;
+            return <Tag color="gold"><SafetyOutlined /> Performance</Tag>;
           case 4:
-            return <Tag color="purple">Conference</Tag>;
+            return <Tag color="purple"><SoundOutlined /> Conference</Tag>;
           case 8:
-            return <Tag color="red">Fundraiser</Tag>;
+            return <Tag color="red"><DollarCircleOutlined /> Fundraiser</Tag>;
           case 16:
-            return <Tag color="blue">Festival</Tag>;
+            return <Tag color="blue"><FireOutlined /> Festival</Tag>;
           case 32:
-            return <Tag color="green">Social Event</Tag>;
+            return <Tag color="green"><TeamOutlined /> Social Event</Tag>;
           default:
             return null;
         }
