7c75751857cbc4e602e9d347d70826413391c5c3
MohammadMostafa RostamKhani <mohammadmostafarostamkhani@gmail.com>
2020-11-16 13:40:53 +0330

    add profile

--- src/view/scripts/pages/profile/Profile.jsx
+++ src/view/scripts/pages/profile/Profile.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import GLogout from "../login/components/GLogout";
-
+import ProfileForm from "./components/ProfileForm";
+import {Row, Col} from 'antd';
 
 const Profile = () =>
 {
   console.log("userToken :" + localStorage.getItem("userToken"));
-  return (<div>
-    <p>Profile</p>
-    <p>{localStorage.getItem("userToken") && <GLogout />}</p>
+  return (<div id="components-form-profile">
+  <Row style={{
+      minHeight: '100vh'
+    }} justify="center" align="middle">
+    <Col span={16}>
+      <ProfileForm />
+    </Col>
+  </Row>
+
     </div>
   );
 };
