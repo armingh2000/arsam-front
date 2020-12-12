f0d63090a7296adf7848d832b58d529d68700c21
Armin <armingh379@gmail.com>
2020-12-12 08:03:23 +0330

    Fix names

--- src/view/scripts/pages/event/eventAdmin/components/EventAdminSidebar.jsx
+++ src/view/scripts/pages/event/eventAdmin/components/EventAdminSidebar.jsx
@@ -30,8 +30,8 @@ const EventAdminSidebar = ({event, dispatch}) =>
       <Menu defaultSelectedKeys={["1"]} theme="light" mode="inline" style={{background:"linear-gradient(to bottom right, rgba(255, 255, 255, 0.89)0%, rgba(247, 252, 252, 0.89)100%)"}}>
         <Avatar size={150} shape="square" style={{margin:"23px", color: 'white', backgroundColor: '#73cac5'}}>{event.event.name}</Avatar>
         <Menu.Item key="1" onClick={() => {setContent("event")}}>Event</Menu.Item>
-        <Menu.Item onClick={() => {setContent("eventMembers")}}>Event Members</Menu.Item>
-        <Menu.Item onClick={() => {setContent("eventRequest")}}>Event Requests</Menu.Item>
+        <Menu.Item onClick={() => {setContent("eventMembers")}}>Members</Menu.Item>
+        <Menu.Item onClick={() => {setContent("eventRequest")}}>Requests</Menu.Item>
       </Menu>
     </div>
   );
