2d410ae1fc5413a8dcfcd9d142ff55739baed04f
Armin Gholampoor <armingh379@gmail.com>
2021-02-05 14:23:49 -0500

    temp

--- src/view/scripts/pages/event/eventAdmin/components/EventAdminSidebar.jsx
+++ src/view/scripts/pages/event/eventAdmin/components/EventAdminSidebar.jsx
@@ -32,17 +32,26 @@ const EventAdminSidebar = ({event, dispatch, eventId}) =>
       <Menu defaultSelectedKeys={[event.adminContent]} defaultOpenKeys={["sub"]} theme="light" mode="inline" style={{background:"linear-gradient(to bottom right, rgba(255, 255, 255, 0.89)0%, rgba(247, 252, 252, 0.89)100%)"}}>
         <Avatar size={150} shape="square" style={{margin:"23px", color: 'white', backgroundColor: '#73cac5'}}>{event.event.name}</Avatar>
           <Menu.Item key="event" onClick={() => {setContent("event")}}>Event</Menu.Item>
-          {isProject && <Menu.Item key="eventMembers" onClick={() => {setContent("eventMembers")}}>Members</Menu.Item>}
-          {isProject && <Menu.Item key="eventRequest" onClick={() => {setContent("eventRequest")}}>Requests</Menu.Item>}
-          {!isProject && <SubMenu title="Event Tickets" key="sub">
-                             <Menu.Item key="eventTickets" onClick={() => {setContent("eventTickets")}}>Tickets</Menu.Item>
-                             <Menu.Item key="eventTicketsManage" onClick={() => {setContent("eventTicketsManage")}}>Manage Tickets</Menu.Item>
-                         </SubMenu>}
-        <Menu.Item key="deleteEvent">
-          <div>
+            {
+                //isProject &&
+            }
+            <Menu.Item key="eventMembers" onClick={() => {setContent("eventMembers")}}>Members</Menu.Item>
+            {
+                //isProject &&
+            }
+            <Menu.Item key="eventRequest" onClick={() => {setContent("eventRequest")}}>Requests</Menu.Item>
+            {
+                //!isProject &&
+            }
+            <SubMenu title="Event Tickets" key="sub">
+            <Menu.Item key="eventTickets" onClick={() => {setContent("eventTickets")}}>Tickets</Menu.Item>
+             <Menu.Item key="eventTicketsManage" onClick={() => {setContent("eventTicketsManage")}}>Manage Tickets</Menu.Item>
+             </SubMenu>
+            <Menu.Item key="deleteEvent">
+            <div>
             <DeleteEventConfirmModal eventId={eventId} dispatch={dispatch}/>
-          </div>
-        </Menu.Item>
+            </div>
+            </Menu.Item>
 
       </Menu>
     </div>
